373dec316a89025800e6ae16e2f9aeb7f34e6bed
Vishal Kardam <vishalat1994@gmail.com>
2021-02-17 18:49:15 +0530

    transition improvement

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,4 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
 
-// Get the navbar
-const navbar = document.getElementById("navigation-bar");
 const headerButton = document.getElementById("headerButton");
 
 const svgCircle = document.getElementById("headerCircle");
@@ -10,25 +6,18 @@ const svgCircle = document.getElementById("headerCircle");
 let centerX = headerButton.offsetLeft + headerButton.offsetWidth / 2;
 let centerY = headerButton.offsetTop + headerButton.offsetHeight / 2;
 
-svgCircle.setAttribute("cx", centerX);
-svgCircle.setAttribute("cy", centerY);
-
-// Get the offset position of the navbar
-const sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+function setSvgCenter() {
+  svgCircle.setAttribute("cx", centerX);
+  svgCircle.setAttribute("cy", centerY);
 }
+setSvgCenter();
 
 headerButton.addEventListener("mouseover", function( event ) {
   svgCircle.setAttribute("r", "4rem");
+  setSvgCenter();
 }, false);
 
 headerButton.addEventListener("mouseleave", function( event ) {
   svgCircle.setAttribute("r", "110%");
+  setSvgCenter();
 }, false);
